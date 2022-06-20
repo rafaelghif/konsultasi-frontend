d6ddba72634f36649f54ad8b4263de57620842c8
rafaelghifari0310 <business.rafaelghifari@gmail.com>
2022-06-21 05:56:56 +0700

    Initial Issue
    Initial Issue

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { Redirect, Route } from 'react-router-dom';
 import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
 import { IonReactRouter } from '@ionic/react-router';
-import Home from './pages/Home';
 
 /* Core CSS required for Ionic components to work properly */
 import '@ionic/react/css/core.css';
@@ -21,6 +20,10 @@ import '@ionic/react/css/display.css';
 
 /* Theme variables */
 import './theme/variables.css';
+import './index.css';
+
+/* Page */
+import Home from './pages/Home';
 
 setupIonicReact();
 
@@ -28,9 +31,7 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/home">
-          <Home />
-        </Route>
+        <Route exact path="/home" component={Home} />
         <Route exact path="/">
           <Redirect to="/home" />
         </Route>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,30 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import './Home.css';
+import { IonContent, IonPage } from "@ionic/react"
+import { Link } from "react-router-dom";
 
 const Home: React.FC = () => {
-  return (
-    <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Blank</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
-    </IonPage>
-  );
-};
+    return (
+        <IonPage>
+            <div className="w-screen bg-slate-100 flex space-x-3 items-center">
+                <div className="px-2 py-3">
+                    <Link to={"/"} className="font-semibold text-lg mx-3">payungkecildesa.id</Link>
+                </div>
+                <div className="flex grow justify-between">
+                    <div className="flex space-x-3 items-center">
+                        <Link to={"/"}>Home</Link>
+                        <Link to={"/"}>Konsultasi</Link>
+                        <Link to={"/"}>Tes Minat Bakat</Link>
+                    </div>
+                    <div className="flex p-2 space-x-2 items-center">
+                        <Link to={"/"}>Daftar</Link>
+                        <Link to={"/"}>Login</Link>
+                    </div>
+                </div>
+            </div>
+            <IonContent>
+                Test
+            </IonContent>
+        </IonPage>
+    )
+}
 
 export default Home;
